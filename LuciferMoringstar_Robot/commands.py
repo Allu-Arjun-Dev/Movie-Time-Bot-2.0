c70ba9517af28dabf74f064c6d1552121ee00cfa
Allu-Arjun-Dev <102848810+Allu-Arjun-Dev@users.noreply.github.com>
2022-04-07 08:46:58 +0530

    Update commands.py

--- LuciferMoringstar_Robot/commands.py
+++ LuciferMoringstar_Robot/commands.py
@@ -13,16 +13,16 @@ async def start_message(bot, message):
         if message.from_user.id not in ADMINS: 
             buttons = [[
              InlineKeyboardButton("🔗 ᗰOᐯIE TIᗰE 2.0", url="https://t.me/+1CZ0RvqgItEzZTQ1"),
-             InlineKeyboardButton("🤖 ᗩᗷOᑌT", callback_data="about") 
+             InlineKeyboardButton("ᗩᗷOᑌT 🤖", callback_data="about") 
              ],[
-             InlineKeyboardButton("🧑‍💻 ᗩᒪᒪᑌ ᗩᖇᒍᑌᑎ", url="https://t.me/helloalluarjun"),
+             InlineKeyboardButton("♻️ ᕼEᒪᑭ", url="https://t.me/helloalluarjun"),
              ]]
         else:
             buttons = [[
              InlineKeyboardButton("🔗 ᗰOᐯIE TIᗰE  2.0", url="https://t.me/+1CZ0RvqgItEzZTQ1"),
-             InlineKeyboardButton("🤖 ᗩᗷOᑌT", callback_data="about") 
+             InlineKeyboardButton("ᗩᗷOᑌT 🤖", callback_data="about") 
              ],[
-             InlineKeyboardButton("🧑‍💻 ᗩᒪᒪᑌ ᗩᖇᒍᑌᑎ", url="https://t.me/helloalluarjun"),
+             InlineKeyboardButton("♻️ ᕼEᒪᑭ", url="https://t.me/helloalluarjun"),
              ]]    
         await message.reply_photo(photo = choice(BOT_PICS), caption=START_MSG.format(mention = message.from_user.mention, bot_name = bot_info.BOT_NAME, bot_username = bot_info.BOT_USERNAME), reply_markup=InlineKeyboardMarkup(buttons))
         
