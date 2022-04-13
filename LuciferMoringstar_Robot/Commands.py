d24d6b133a3282b5db39efde11383a9be9eadc3d
Allu-Arjun-Dev <102848810+Allu-Arjun-Dev@users.noreply.github.com>
2022-04-14 01:23:25 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -12,7 +12,7 @@ async def start_message(bot, message):
     if len(message.command) != 2:
         if message.from_user.id not in ADMINS: 
             buttons = [[
-             InlineKeyboardButton("Movie Time 2.0", url=f"https://t.me/+1CZ0RvqgItEzZTQ1")
+             InlineKeyboardButton("🔗 Movie Time 2.0", url=f"https://t.me/+1CZ0RvqgItEzZTQ1")
              ],[
              InlineKeyboardButton("ℹ️ Help", callback_data="help"),
              InlineKeyboardButton("😎 About", callback_data="about") 
@@ -22,7 +22,7 @@ async def start_message(bot, message):
              ]]
         else:
             buttons = [[
-             InlineKeyboardButton("Movie Time 2.0", url=f"https://t.me/+1CZ0RvqgItEzZTQ1")
+             InlineKeyboardButton("🔗 Movie Time 2.0", url=f"https://t.me/+1CZ0RvqgItEzZTQ1")
              ],[
              InlineKeyboardButton("ℹ️ Help", callback_data="bot_owner"),
              InlineKeyboardButton("😎 About", callback_data="about") 
